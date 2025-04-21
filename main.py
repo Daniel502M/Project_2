798861d333dea3e57d220648df68a1808495721d
Daniel <dm5022001@gmail.com>
2025-04-21 23:09:45 +0400

    install requirements libraries

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
 import pygame
+import sys
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    pygame.init()
+    try:
+        main()
+    except Exception as e:
+        print("Произошла ошибка:", e)
+    finally:
+        pygame.quit()
+        sys.exit()
